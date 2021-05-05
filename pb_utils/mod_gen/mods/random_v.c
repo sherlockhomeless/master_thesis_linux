009db73da5273c16acfa1cbd9233b3f4d96f5352
sherlockhomeless <manuel.leppert@haw-hamburg.de>
2021-05-05 11:08:38 +0200

    kelvin makefile adjusted

---
+++ pb_utils/mod_gen/mods/random_v.c
@@ -0,0 +1,60 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/kthread.h>
+#include <../kernel/sched/sched.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("PB Scheduler - Module that sets a proxy task and scheduling plan");
+
+static int loop_thread_func(void *data)
+{
+        unsigned int c = 0;
+        while (!kthread_should_stop()) {
+                int a = 0;
+                set_current_state(TASK_INTERRUPTIBLE);
+                for (;a < 200000; a++){}
+                schedule();
+                c++;
+        }
+        return 0;
+}
+
+static void init_rq(struct pb_rq *pb_rq)
+{
+    set_pb_plan_size(pb_rq, 2);
+    set_pb_plan_entry(pb_rq, 0, 3600000000000, 2000000000);
+    set_pb_plan_entry(pb_rq, 1, 1000000000, 0);
+    
+}
+
+static int __init pb_client_init(void)
+{
+    struct task_struct *proxy_task;
+    struct rq *rq;
+
+    proxy_task = kthread_create(loop_thread_func, NULL, "PB proxy thread");
+    proxy_task->sched_class = &pb_sched_class;
+
+    rq = this_rq();
+    init_rq(&rq->pb);
+
+    rq->pb.proxy_task = proxy_task;
+
+    return 0;
+}
+
+static void __exit pb_client_cleanup(void)
+{
+    struct rq *rq;
+    rq = this_rq();
+
+    // set pb_rq back to initial values
+    init_pb_rq(&rq->pb);
+
+    printk(KERN_DEBUG "Cleaning up module.\n");
+}
+
+module_init(pb_client_init);
+module_exit(pb_client_cleanup);

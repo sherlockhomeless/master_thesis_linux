fd6644ae5d8a3e9b968b2c582628813973ef6519
Vagrant Default User <vagrant@pbs-vm>
2021-05-05 10:12:15 +0200

    code runs schedule() without problem

--- kernel/sched/prediction_failure_handling.c
+++ kernel/sched/prediction_failure_handling.c
@@ -1,15 +1,14 @@
 #include "sched.h"
 
 
-struct PBS_Plan pbs_plan ={0};
+struct PBS_Plan pbs_plan = {0};
+struct PBS_Plan* pbs_plan_ptr = &pbs_plan;
 
 
 struct PBS_Plan* get_pbs_plan(void){
-    return &pbs_plan;
+    printk(KERN_INFO "[PBS_get_pbs_plan]: pbs_plan_address is %p\n", pbs_plan_ptr);
+    if (pbs_plan_ptr == NULL)
+        printk(KERN_ALERT "[PBS_get_pbs_plan]: Plan is null\n");
+    return pbs_plan_ptr;
 }
 EXPORT_SYMBOL(get_pbs_plan);
-
-extern void print_from_subdir(void){
-
-}
-EXPORT_SYMBOL(print_from_subdir);
